b53fec92aed48dda0608eb2160394ce4c1ab71d7
Fischer, Patrick (SE-A/35) <patrick.fischer@volkswagen.de>
2024-03-09 17:29:43 +0100

    extracted readDraftCards(), reduced printDraftCards(), acccounted for breakValue and guardValue in calculateBattleValue(), accounted for canCharge in calculateDraftValue(), included battleValue into print(), reduced printState(), included att > 0 into attack decision

--- LoCaM/main.cc
+++ LoCaM/main.cc
@@ -12,12 +12,12 @@ struct Card;
 struct Action;
 struct State;
 
+vector<Card> readDraftCards();
 State readAndSortBattleState();
 pair<Player, Player> readPlayers();
 vector<Action> readOpponentsActions();
 tuple<vector<Card>, vector<Card>, vector<Card>> readCards();
-void printState(const Player& OPPONENT, const vector<Card>& OPPONENTS_BOARD, const vector<Action>& OPPONENTS_ACTIONS, 
-        const Player& PLAYER, const vector<Card>& PLAYERS_HAND, const vector<Card>& PLAYERS_BOARD);
+void printDraftCards(const vector<Card>& DRAFT_CARDS);
 void printState(const State& STATE);
 int findMaxCost(const vector<Card>& CARDS);
 vector<Card> findComboToUseMostMana(int mana, const vector<Card>& PLAYERS_HAND);
@@ -48,8 +48,8 @@ struct Card {
     int cost;
     int att;
     int def;
-    bool canBreak = false;
     bool canAttack = false;
+    bool canBreak = false;
     bool canGuard = false;
 
     Card(int instanceId, int cost, int att, int def) {
@@ -59,18 +59,20 @@ struct Card {
         this->def = def;
     }
 
-    // TODO with effects
-    int calculateBattleValue() const {
-        return this->att;
+    double calculateBattleValue() const {
+        int breakValue = this->canBreak ? 1 : 0;
+        double guardValue = this->canGuard ? 1 + (def - 1) * 0.2 : 0;
+        return this->att + breakValue + guardValue;
     }
     double calculateDraftValue(const map<int, int>& PLAYERS_DECK_STRUCTURE) const {
         double divider = cost == 0 ? 0.5 : cost;
-        return (this->calculateBattleValue() + this->def) / divider;
+        return (this->calculateBattleValue() + (this->canAttack ? 1 : 0) + this->def) / divider;
     }
     void print() const {
         cerr << "Card [ instanceId: " << setw(3) << instanceId << ", cost: " << setw(2) << cost 
                 << ", att: " << setw(2) << att << ", def: " << setw(3) << def << ", canAttack: " << canAttack
                 << (canBreak ? ", B" : "") << (canGuard ? ", G" : "") 
+                << ", battleValue: " << calculateBattleValue()
                 << " ]" << endl;
     }
 };
@@ -114,17 +116,26 @@ struct State {
             playersHand(playersHand), playersBoard(playersBoard) {}
 };
 
+vector<Card> readDraftCards() {
+    readPlayers();
+    readOpponentsActions();
+    auto [ opponentsBoard, playersHand, playersBoard ] = readCards();
+    return playersHand;
+}
 State readAndSortBattleState() {
     auto [ player, opponent ] = readPlayers();
     vector<Action> opponentsActions = readOpponentsActions();
     auto [ opponentsBoard, playersHand, playersBoard ] = readCards();
+
     sort(opponentsBoard.begin(), opponentsBoard.end(), 
             [](Card a, Card b) { return a.calculateBattleValue() > b.calculateBattleValue(); });
+    sort(opponentsBoard.begin(), opponentsBoard.end(), 
+            [](Card a, Card b) { return a.canGuard > b.canGuard; });
     sort(playersHand.begin(), playersHand.end(), [](Card a, Card b) { return a.cost > b.cost; });
     sort(playersBoard.begin(), playersBoard.end(), 
             [](Card a, Card b) { return a.calculateBattleValue() < b.calculateBattleValue(); });
-    for_each(playersBoard.begin(), playersBoard.end(), [](Card a) { a.canAttack = true; });
 
+    for_each(playersBoard.begin(), playersBoard.end(), [](Card a) { a.canAttack = true; });
     return State(player, opponent, opponentsActions, opponentsBoard, playersHand, playersBoard);
 }
 pair<Player, Player> readPlayers() {
@@ -219,50 +230,28 @@ tuple<vector<Card>, vector<Card>, vector<Card>> readCards() {
     }
     return tuple(opponentsBoard, playersHand, playersBoard);
 }
-void printState(const Player& OPPONENT, const vector<Card>& OPPONENTS_BOARD, const vector<Action>& OPPONENTS_ACTIONS, 
-        const Player& PLAYER, const vector<Card>& PLAYERS_HAND, const vector<Card>& PLAYERS_BOARD) {
-    cerr << "opponent:" << endl;
-    OPPONENT.print();
-
-    cerr << endl << "opponentsBoard:" << endl;
-    for (const Card& CARD : OPPONENTS_BOARD) {
-        CARD.print();
-    }
-
-    cerr << endl << "opponentsActions:" << endl;
-    for (const Action& ACTION : OPPONENTS_ACTIONS) {
-        ACTION.print();
-    }
-
-    cerr << endl << "player:" << endl;
-    PLAYER.print();
-
-    cerr << endl << "playersHand:" << endl;
-    for (const Card& CARD : PLAYERS_HAND) {
-        CARD.print();
-    }
-
-    cerr << endl << "playersBoard:" << endl;
-    for (const Card& CARD : PLAYERS_BOARD) {
+void printDraftCards(const vector<Card>& DRAFT_CARDS) {
+    cerr << "draftCards:" << endl;
+    for (const Card& CARD : DRAFT_CARDS) {
         CARD.print();
     }
 }
 void printState(const State& STATE) {
-    cerr << "opponent:" << endl;
-    STATE.opponent.print();
+    // cerr << "opponent:" << endl;
+    // STATE.opponent.print();
 
-    cerr << endl << "opponentsBoard:" << endl;
+    cerr /* << endl */ << "opponentsBoard:" << endl;
     for (const Card& CARD : STATE.opponentsBoard) {
         CARD.print();
     }
 
-    cerr << endl << "opponentsActions:" << endl;
-    for (const Action& ACTION : STATE.opponentsActions) {
-        ACTION.print();
-    }
+    // cerr << endl << "opponentsActions:" << endl;
+    // for (const Action& ACTION : STATE.opponentsActions) {
+    //     ACTION.print();
+    // }
 
-    cerr << endl << "player:" << endl;
-    STATE.player.print();
+    // cerr << endl << "player:" << endl;
+    // STATE.player.print();
 
     cerr << endl << "playersHand:" << endl;
     for (const Card& CARD : STATE.playersHand) {
@@ -285,13 +274,11 @@ int findMaxCost(const vector<Card>& CARDS) {
 }
 vector<Card> findComboToUseMostMana(int mana, const vector<Card>& PLAYERS_HAND) {
     for (int manaToBeUsed = mana; manaToBeUsed >= 0; manaToBeUsed--) {
-        cerr << "manaToBeUsed: " << manaToBeUsed << endl;
         for (int cardIndexToStart = 0; cardIndexToStart < PLAYERS_HAND.size(); cardIndexToStart++) {
             vector<Card> combo;
             int manaLeft = manaToBeUsed;
             for (int cardIndex = cardIndexToStart; cardIndex < PLAYERS_HAND.size(); cardIndex++) {
                 const Card& CARD = PLAYERS_HAND[cardIndex];
-                CARD.print();
                 if (CARD.cost <= manaLeft) {
                     combo.emplace_back(CARD);
                     manaLeft -= CARD.cost;
@@ -324,16 +311,14 @@ int main() {
 
     // DRAFT
     while (playersDeckCards < 30) {
-        auto [ player, opponent ] = readPlayers();
-        vector<Action> opponentsActions = readOpponentsActions();
-        auto [ opponentsBoard, playersHand, playersBoard ] = readCards();
-        printState(opponent, opponentsBoard, opponentsActions, player, playersHand, playersBoard);
+        vector<Card> draftCards = readDraftCards();
+        printDraftCards(draftCards);
         double maxCardRating = 0;
         int cardNumberToPick = -1;
 
         cerr << "cardRatings: ";
         for (int cardNumber = 0; cardNumber < 3; cardNumber++) {
-            const Card& CARD = playersHand[cardNumber];
+            const Card& CARD = draftCards[cardNumber];
             double cardRating = CARD.calculateDraftValue(playersDeckStructure);
             cerr << cardRating << ", ";
             if (cardRating > maxCardRating) {
@@ -355,7 +340,7 @@ int main() {
         playCombo(COMBO, state.playersBoard);
 
         for (Card& playersCard : state.playersBoard) {
-            if (playersCard.canAttack) {
+            if (playersCard.canAttack && playersCard.att > 0) {
                 for (Card& opponentsCard : state.opponentsBoard) {
                     if (opponentsCard.def > 0) {
                         cout << "ATTACK " << playersCard.instanceId << " " << opponentsCard.instanceId << " DIE!;";
